eef473cd730f12f76cc0a48b30b807ea4b392f55
Sudha Lalwani <sudha0lalwani@gmail.com>
2024-03-21 00:55:09 +0530

    Changes

--- Leetcode-75/1431 Kids With the Greatest Number of Candies.java
+++ Leetcode-75/1431 Kids With the Greatest Number of Candies.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = 0;
--- Leetcode-75/1768 Merge Strings Alternately.java
+++ Leetcode-75/1768 Merge Strings Alternately.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class MSA {
+class MSA {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter word1: ");
