877ec4b4ab396ce19f0cc3abdab45a63dd22a6cb
Sudha Lalwani <sudha0lalwani@gmail.com>
2024-03-21 00:25:55 +0530

    Added solution for Merge String Alternatively

---
+++ 1768. Merge Strings Alternately/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter word1: ");
+        String word1 = sc.nextLine();
+        System.out.println("Enter word2: ");
+        String word2 = sc.next();
+        System.out.println(mergeAlternately(word1, word2));
+        sc.close();
+    }
+    public static String mergeAlternately(String word1, String word2) {
+        String output = "";
+        int i;
+        String remain  = "";
+        if(word1.length() > word2.length()){
+            i = word2.length();
+            remain = word1.substring(i, word1.length());
+        }else if(word1.length() < word2.length()){
+            i = word1.length();
+            remain = word2.substring(i, word2.length());
+        }else{
+            i = word1.length();
+            remain = "";
+        }
+        for(int l = 1; l< i+1; l++){
+            output = output + word1.substring(l-1, l);
+            output = output + word2.substring(l-1, l);
+        }
+        return output + remain;
+    }
+}
